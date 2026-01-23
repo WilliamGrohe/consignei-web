c1ecb32605136d3995261096995da97f5c81ff80
WilliamGrohe <william.grohe@gmail.com>
2026-01-23 02:47:15 -0300

    refactored card of create book

--- src/pages/CreateBook.tsx
+++ src/pages/CreateBook.tsx
@@ -1,6 +1,3 @@
-// import { useState } from "react";
-// import { createBook } from "../api/books";
-// import type { CreateBookProps } from "../types/book";
 import { useForm } from "react-hook-form";
 
 import { Input } from "@/components/ui/input";
@@ -10,16 +7,15 @@ import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createBook } from "@/api/books";
-
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 
 const createBookSchema = z.object({
   title: z.string(),
   isbn: z.string(),
   cover_price: z.coerce.number(),
-})
+});
 
 type CreateBookSchema = z.infer<typeof createBookSchema>;
-
 
 export function CreateBook() {
   const { register, handleSubmit } = useForm<CreateBookSchema>({
@@ -29,50 +25,59 @@ export function CreateBook() {
   async function handleCreateBook(data: CreateBookSchema) {
     console.log(data);
     try {
-      await createBook(data)
-      alert('Livro cadastrado com sucesso!')
-    }catch (error) {
-      alert('Erro ao cadastrar livro. ' + error)
+      await createBook(data);
+      alert("Livro cadastrado com sucesso!");
+    } catch (error) {
+      alert("Erro ao cadastrar livro. " + error);
     }
-    
   }
 
   return (
     <div style={{ padding: 24, maxWidth: 480 }}>
-      <h1>Cadastrar Livro</h1>
+      <Card className="w-full max-w-sm p-4">
+        <CardHeader>
+          <CardTitle className="text-2xl">Cadastrar Novo Livro</CardTitle>
+        </CardHeader>
 
-      <form onSubmit={handleSubmit(handleCreateBook)}>
-        <FieldGroup>
-          <Field>
-            <FieldLabel htmlFor="fieldgroup-title">Título</FieldLabel>
-            <Input
-              {...register("title")}
-              placeholder="Título do livro"
-              required
-            />
-          </Field>
+        <form onSubmit={handleSubmit(handleCreateBook)}>
+          <FieldGroup>
+            <Field>
+              <FieldLabel htmlFor="fieldgroup-title">Título</FieldLabel>
+              <Input
+                {...register("title")}
+                placeholder="Título do livro"
+                required
+              />
+            </Field>
 
-          <Field>
-            <FieldLabel htmlFor="fieldgroup-isbn">Código ISBN</FieldLabel>
-            <Input {...register("isbn")} placeholder="978662993..." required />
-          </Field>
+            <Field>
+              <FieldLabel htmlFor="fieldgroup-isbn">Código ISBN</FieldLabel>
+              <Input
+                {...register("isbn")}
+                placeholder="978662993..."
+                required
+              />
+            </Field>
 
-          <Field>
-            <FieldLabel htmlFor="fieldgroup-isbn">Preço de Capa</FieldLabel>
-            <Input
-              {...register("cover_price")}
-              placeholder="49,90"
-              type="number"
-              step={0.01}
-              required
-            />
-          </Field>
+            <Field>
+              <FieldLabel htmlFor="fieldgroup-isbn">Preço de Capa</FieldLabel>
+              <Input
+                {...register("cover_price")}
+                placeholder="49,90"
+                type="number"
+                step={0.01}
+                required
+              />
+            </Field>
 
-          <Field >
-            <Button type="submit" variant="outline">Cadastrar</Button>
-          </Field>
-        </FieldGroup>
-      </form>
+            <Field>
+              <Button type="submit">
+                Cadastrar
+              </Button>
+            </Field>
+          </FieldGroup>
+        </form>
+      </Card>
     </div>
   );
 }

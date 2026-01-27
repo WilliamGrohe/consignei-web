94b3bef5b3ccdbfd166d4ec963807eacaa5c1a07
WilliamGrohe <william.grohe@gmail.com>
2026-01-27 01:02:03 -0300

    refactor: remove commented-out code from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,3 @@
-// import './App.css'
-// import { ListBooks } from './components/listBooks'
-// import { CreateBook } from './pages/CreateBook'
-// import Dashboard from './pages/Dashboard'
-// import { Partners } from './pages/partners'
-// import { CreatePartner } from './components/createPartner'
-
-
-// export function App() {
-//   return (
-//     <div className="">
-//       <Dashboard />
-//       <CreateBook />  
-//       <ListBooks />
-//       <Partners />
-//       <CreatePartner />
-//     </div>
-// )
-// }
-
 import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
